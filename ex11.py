7c960ef263107a20a27886d43fa49c3277feee40
Robert Fryman <robert.fryman@gmail.com>
2014-07-09 16:25:38 -0500

    done with 11-15

---
+++ ex11.py
@@ -0,0 +1,14 @@
+# Exercise 11. Asking Questions
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So you're %r old, %r tall and %r heavy." %(
+      age, height, weight)
+
+#raw_input is a built in python function that is written to standard output
+#it converts input into a string(stripping a trailing newline), and returns it. 

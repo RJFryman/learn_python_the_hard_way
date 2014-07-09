20a278bf78c529fa26265da0de6c8c65f249f8dc
Robert Fryman <robert.fryman@gmail.com>
2014-07-09 13:58:40 -0500

    First 5 exercises done.

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# Exercise 1. A Good First Program
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing/"
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "Testing that all things work."
+# This is a comment, Anything after the # is ignored by python.

f4574582801132a91008e9e992798964f7af3c22
Robert Fryman <robert.fryman@gmail.com>
2014-07-09 21:49:18 -0500

    added 21-25

---
+++ ex22.py
@@ -0,0 +1,42 @@
+# Exercise 22. What Do You Know So Far?
+
+# print : outputs results of expressions
+# python : run script using python
+# # : comment out part of a script
+# + : addition, or concatenate
+# - : subtraction
+# / : division
+# * : multipplication
+# % : modulo operation
+# > : greater than
+# < : less than
+# >= : greater than or equal to
+# <= : less than or equal to
+# = : equals, names variable
+# %d : number or decimal
+# %r : raw data
+# %s : string
+# \n : newline
+# \t : tab
+# \ : escape
+# \\ : escape
+# """ : string, as many lines of text as you want
+# ''' : string, as many lines of text as you want
+# raw_input : read a string from input
+# pydoc : python documentation
+# open : open a file
+# file : open or create a file
+# os : operating system
+# sys : built-in module
+# import : find/initialze a module
+# argv : The list of command line arguments passed to a Python script.
+# read : read a file's contents
+# close : close a file
+# readline : Read one entire line from the file.
+# truncate : Empties the file
+# write : writes stuff to file
+# def : start a function
+# *args : pass an argument list, like argvs for functions
+# seek : set a file's position
+# += : delimiter, variable equals itself plus integer to thr right of an =
+# return : You can return anything that you can put to the right of an =
